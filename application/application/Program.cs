aced8f9288ff7f26d442084e726a1516ff0f6a8c
trueberryless <felix@schneider-it.at>
2023-03-31 10:13:55 +0200

    level1

--- application/application/Program.cs
+++ application/application/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
+            Count0("level1_1.in", "level1_1.out");
+            Count0("level1_2.in", "level1_2.out");
+            Count0("level1_3.in", "level1_3.out");
+            Count0("level1_4.in", "level1_4.out");
+            Count0("level1_5.in", "level1_5.out");
+            Count0("level1_example.in", "level1_example.out");
+        }
+
+        public static void Count0(string filename, string outputname)
+        {
+            string content = Read(filename);
+            string[] lines = content.Split("\n");
+            int countline1 = 0;
+            foreach (var item in lines[1])
+            {
+                if (item == 'O')
+                {
+                    countline1++;
+                }
+            }
+
+            countline1 *= lines.Length - 1;
             
+            Write(outputname, countline1.ToString());
         }
 
         public static string Read(string filename)
