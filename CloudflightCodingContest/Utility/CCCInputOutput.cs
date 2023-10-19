5d74970005cfd1922bb1caeb7c2f0692d782496e
DaSchmitzi <87095537+DaSchmitzi@users.noreply.github.com>
2023-10-19 21:55:20 +0200

    created Utility-Project

---
+++ CloudflightCodingContest/Utility/CCCInputOutput.cs
@@ -0,0 +1,19 @@
+﻿using static Utility.FileReaderWriter;
+
+namespace Utility;
+
+public class CCCInputOutput
+{
+    private static string projectFolderRelativePath = @"..\..\..\..";
+    public static string Input(string filename)
+    {
+        string path = Path.Combine(projectFolderRelativePath, "input", filename);
+        return FileReaderWriter.Read(path);
+    }
+
+    public static void Output(string filename, string output)
+    {
+        string path = Path.Combine(projectFolderRelativePath, "output", filename);
+        FileReaderWriter.Write(path, output);
+    }
+}
---
+++ CloudflightCodingContest/Utility/FileReaderWriter.cs
@@ -0,0 +1,36 @@
+﻿namespace Utility;
+
+public static class FileReaderWriter
+{
+    public static string Read(string path)
+    {
+        string content = "";
+        using (StreamReader sr = new StreamReader(path))
+        {
+            content = sr.ReadToEnd();
+        }
+
+        return content;
+    }
+
+    public static void Write(string path, string content)
+    {
+        using (StreamWriter sw =
+               new StreamWriter(path))
+        {
+            sw.Write(content);
+        }
+    }
+
+    public static void Append(string path, string content)
+    {
+        using FileStream fs =
+            new FileStream(
+                path,
+                FileMode.Append, FileAccess.Write);
+        using (StreamWriter sw = new StreamWriter(fs))
+        {
+            sw.Write(content);
+        }
+    }
+}
