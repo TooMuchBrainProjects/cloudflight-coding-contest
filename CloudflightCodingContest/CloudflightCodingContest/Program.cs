8e7336fe3d47cf4a3d0454afde491f5c55bc60d1
trueberryless <trueberryless+github@gmail.com>
2023-10-19 16:16:55 +0200

    template

---
+++ CloudflightCodingContest/CloudflightCodingContest/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace CloudflightCodingContest;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string fileContent = Read("test.txt");
+        Console.WriteLine(fileContent);
+        Write("test.txt", fileContent);
+        Append("test.txt", fileContent);
+    }
+    
+    public static string Read(string filename)
+    {
+        string content = "";
+        using (StreamReader sr =
+               new StreamReader(Path.Combine(@"..\..\..\input",
+                   filename)))
+        {
+            content = sr.ReadToEnd();
+        }
+
+        return content;
+    }
+
+    public static void Write(string filename, string content)
+    {
+        using (StreamWriter sw =
+               new StreamWriter(Path.Combine(@"..\..\..\output",
+                   filename)))
+        {
+            sw.Write(content);
+        }
+    }
+
+    public static void Append(string filename, string content)
+    {
+        using FileStream fs =
+            new FileStream(
+                Path.Combine(@"..\..\..\output", filename),
+                FileMode.Append, FileAccess.Write);
+        using (StreamWriter sw = new StreamWriter(fs))
+        {
+            sw.Write(content);
+        }
+    }
+}
