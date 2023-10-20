ff775b2f50c90ca6d295905c93499afe14f3c7ff
DaSchmitzi <87095537+DaSchmitzi@users.noreply.github.com>
2023-10-20 09:50:30 +0200

    Preparation

--- CloudflightCodingContest/CloudflightCodingContest/Program.cs
+++ CloudflightCodingContest/CloudflightCodingContest/Program.cs
@@ -1,47 +1,25 @@
-﻿namespace CloudflightCodingContest;
+﻿using Utility;
+
+namespace CloudflightCodingContest;
 
 public class Program
 {
+    private static string projectFolderRelativePath = @"..\..\..\..";
+    private static void ReadExecuteWrite(string inputFilename)
+    {
+        string outputFilename = inputFilename.Replace(".in", ".out");
+        CCCInputOutput.Output(outputFilename, LevelExecution(CCCInputOutput.Input(inputFilename)));
+    }
     static void Main(string[] args)
     {
-        string fileContent = Read("test.txt");
-        Console.WriteLine(fileContent);
-        Write("test.txt", fileContent);
-        Append("test.txt", fileContent);
+        //ReadExecuteWrite(filename of inputFile); eg. ReadExecuteWrite("level2_3.in");
+        ReadExecuteWrite("test.txt");
+    }
+
+    private static string LevelExecution(string input)
+    {
+        string output = "test";
+        return output;
     }
     
-    public static string Read(string filename)
-    {
-        string content = "";
-        using (StreamReader sr =
-               new StreamReader(Path.Combine(@"..\..\..\input",
-                   filename)))
-        {
-            content = sr.ReadToEnd();
-        }
-
-        return content;
-    }
-
-    public static void Write(string filename, string content)
-    {
-        using (StreamWriter sw =
-               new StreamWriter(Path.Combine(@"..\..\..\output",
-                   filename)))
-        {
-            sw.Write(content);
-        }
-    }
-
-    public static void Append(string filename, string content)
-    {
-        using FileStream fs =
-            new FileStream(
-                Path.Combine(@"..\..\..\output", filename),
-                FileMode.Append, FileAccess.Write);
-        using (StreamWriter sw = new StreamWriter(fs))
-        {
-            sw.Write(content);
-        }
-    }
 }
